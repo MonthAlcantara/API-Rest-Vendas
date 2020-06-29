e467f9ec1b0d0a8ea8c8f2b6a7729c3588137f56
Montival Junior <montival_junior@yahoo.com.br>
2020-06-29 11:49:48 -0300

    Criadas classes Controllers

---
+++ src/main/java/io/github/monthalcantara/endpoint/ClientController.java
@@ -0,0 +1,9 @@
+package io.github.monthalcantara.endpoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+}
---
+++ src/main/java/io/github/monthalcantara/endpoint/ProductController.java
@@ -0,0 +1,9 @@
+package io.github.monthalcantara.endpoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+}
