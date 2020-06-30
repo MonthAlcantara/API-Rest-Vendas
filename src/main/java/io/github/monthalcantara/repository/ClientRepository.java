65f07c517c3186ca34cc8549b03c3e7c36e5b3f8
Montival Junior <montival_junior@yahoo.com.br>
2020-06-30 12:51:20 -0300

    Criação de metodos ClientRepository

--- src/main/java/io/github/monthalcantara/repository/ClientRepository.java
+++ src/main/java/io/github/monthalcantara/repository/ClientRepository.java
@@ -2,13 +2,24 @@ package io.github.monthalcantara.repository;
 
 import io.github.monthalcantara.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
+    Optional<List<Client>> findByName(String name);
 
-    Optional<Client> findByIdOrName(Integer integer, String name);
+    void deleteByName(String name);
+
+    boolean existsByName(String name);
+
+    @Query("Select c from Client c left join fetch c.orderItems where c.id = :id")
+    Optional<Client> findClientFetchOrderItem(@Param("id") Integer id);
+
+
 }
