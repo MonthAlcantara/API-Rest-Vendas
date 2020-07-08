e6483bd0e330da0a76b7aa96e2505eb4a338d6a5
Montival Junior <montival_junior@yahoo.com.br>
2020-07-08 20:59:47 -0300

    Criada classe atualizaStatusDtO

---
+++ src/main/java/io/github/monthalcantara/dto/request/UpdateOrderStatusDTO.java
@@ -0,0 +1,11 @@
+package io.github.monthalcantara.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateOrderStatusDTO {
+
+    private String newStatus;
+}
