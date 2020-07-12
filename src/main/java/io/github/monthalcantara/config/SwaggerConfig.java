e257b1816503aa40259510e5099917f8e19d9b8f
Montival Junior <montival_junior@yahoo.com.br>
2020-07-12 12:50:45 -0300

    Configuração do Swagger

---
+++ src/main/java/io/github/monthalcantara/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package io.github.monthalcantara.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors
+                .basePackage("io.monthalcantara.endpoint"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("Api de projeto de vendas")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+private Contact contact(){
+        return new Contact("Montival Junior",
+                "monthalcantara.github.io",
+                "montival_junior@yahoo.com.br");
+}
+}
