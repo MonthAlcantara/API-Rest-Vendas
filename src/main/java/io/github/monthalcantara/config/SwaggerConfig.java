049b4986703aeef3c3f41685db3bca2acaa24412
Montival Junior <montival_junior@yahoo.com.br>
2020-07-12 13:30:33 -0300

    Configuração utilização do token no Swagger

--- src/main/java/io/github/monthalcantara/config/SwaggerConfig.java
+++ src/main/java/io/github/monthalcantara/config/SwaggerConfig.java
@@ -5,28 +5,35 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket docket(){
+    public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors
-                .basePackage("io.monthalcantara.endpoint"))
+                        .basePackage("io.github.monthalcantara.endpoint"))
                 .paths(PathSelectors.any())
                 .build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .apiInfo(apiInfo());
     }
-    private ApiInfo apiInfo(){
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Vendas API")
                 .description("Api de projeto de vendas")
@@ -34,9 +41,33 @@ public class SwaggerConfig {
                 .contact(contact())
                 .build();
     }
-private Contact contact(){
+
+    private Contact contact() {
         return new Contact("Montival Junior",
                 "monthalcantara.github.io",
                 "montival_junior@yahoo.com.br");
-}
+    }
+
+    public ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(reference);
+        return auths;
+    }
 }
